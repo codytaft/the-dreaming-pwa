b82cb856fbf479697b01f2e749ae4247245f0c72
Cody Taft <cody.taft@gmail.com>
2019-02-24 17:27:39 -0700

    Edit name of site

--- components/head.js
+++ components/head.js
@@ -11,7 +11,7 @@ const Head = props => (
     <meta charSet="UTF-8" />
     <title>{props.title || ''}</title>
     <meta
-      name="description"
+      name="The Dreaming PWA"
       content={props.description || defaultDescription}
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
